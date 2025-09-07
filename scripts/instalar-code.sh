11947681b1543f223a2911388f2d97926bc011e0
derivescript <danielcfaria@gmail.com>
2025-09-07 04:50:29 -0400

    Create instalar-code.sh

---
+++ scripts/instalar-code.sh
@@ -0,0 +1,13 @@
+!#/bin/sh
+
+sudo apt update
+sudo apt install -y curl apt-transport-https
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+
+#Adiciona o repositório
+echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+#Procede a instalação
+sudo apt update
+sudo apt install code
